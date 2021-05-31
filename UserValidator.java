6ae2b0e4567a45a017f7cb379f456afcbf9c76ba
AnushaManda-27 <anushamanda27@gmail.com>
2021-05-31 12:53:57 +0530

    Added:Day 10 User Regestration

--- UserValidator.java
+++ UserValidator.java
@@ -1,54 +1,38 @@
-
+package userreg;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-    public class UserValidator {
-        private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{3,20}$" ;
-        private static final String LAST_NAME_PATTERN = "^[A-Z]{1}[a-z]{3,20}$" ;
-        private static final String EMAIL_ADDRESS_PATTERN = "^[a-z]{3}[.][a-z]{3}+[@][a-z]{5,10}[.][a-z]{2}[.][a-z]{2}$" ;
-        private static final String MOBILE_NUMBER_PATTERN = "^[6-9]{1}[0-9]{9,9}$" ;
-
-        //FirstName Validation check
-        public boolean validateFirstName(String FirstName) {
-            Pattern p = Pattern.compile(FIRST_NAME_PATTERN) ;
-            if (FirstName == null) {
-                return false;
-            }
-            Matcher m = p.matcher(FIRST_NAME_PATTERN);
-            return true;
-
-        }
-
-        //LastName Validation check
-        public boolean validateLastName(String LastName) {
-            Pattern p = Pattern.compile(LAST_NAME_PATTERN) ;
-            if (LastName == null) {
-                return false;
-            }
-            Matcher m = p.matcher(LAST_NAME_PATTERN);
-            return true;
-        }
+public class UserValidator1 {
+    private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+    private static final String LAST_NAME_PATTERN = "^[A-Z]{1}[a-z]{3,20}$" ;
+    private static final String EMAIL_ADDRESS_PATTERN = "^(?=.{1,64}@)[A-Z a-z]+(\\\\.[A-Z a-z 0-9_-]+)@[A-Z a-z 0-9_-]+(\\\\.[A-Za-z0-9-]+)(\\\\.[A-Za-z]{2,})$";
+    private static final String MOBILE_NUMBER_PATTERN = "^[7-9]{1}[0-9]{9,9}$" ;
+    private static final String PASSWORD_PATTERN = "^(?=.[0-9])(?=.[a-z])(?=.[A-Z])(?=.[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$" ;
 
 
-        //EmailValidation check
-        public boolean validateEMAIL_ADDRESS_PATTERN(String Email) {
-            Pattern p = Pattern.compile(EMAIL_ADDRESS_PATTERN) ;
-            if (Email == null) {
-                return false;
-            }
-            Matcher m = p.matcher(EMAIL_ADDRESS_PATTERN);
-            return true;
-        }
 
-        //Mobile Validation check
-        public boolean validateMOBILE_NUMBER_PATTERN(String Mobile) {
-            Pattern p = Pattern.compile(MOBILE_NUMBER_PATTERN) ;
-            if (Mobile == null) {
-                return false;
-            }
-            Matcher m = p.matcher(MOBILE_NUMBER_PATTERN);
-            return true;
-        }
-
+    public boolean validateFirstName(String fname) {
+        Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
+        return pattern.matcher(fname).matches();
     }
 
+    public boolean validateLastName(String lname) {
+        Pattern pattern = Pattern.compile(LAST_NAME_PATTERN);
+        return pattern.matcher(lname).matches();
+    }
+
+
+    public boolean validateEmailAddress(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_ADDRESS_PATTERN);
+        return pattern.matcher(email).matches();
+    }
+
+    public boolean validateMobileNumber(String mobile) {
+        Pattern pattern = Pattern.compile(MOBILE_NUMBER_PATTERN);
+        return pattern.matcher(mobile).matches();
+    }
+
+    public boolean validatePassword(String password) {
+        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+        return pattern.matcher(password).matches();
+    }
+}
